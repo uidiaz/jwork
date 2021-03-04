3d01bf3272a4b95cbd173dad9cf3cb1c1154d879
mailmanoriginal <mailmanoriginal@gmail.com>
2021-03-04 17:19:12 +0700

    CS-Modul1

--- JWork.java
+++ JWork.java
@@ -7,32 +7,9 @@
  */
 public class jwork
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class jwork
-     */
-    public jwork()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    public void main(String[] args) 
+    public void main(String [] args) 
     { 
-        System.out.println("Hello World"); 
+        
     } 
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
