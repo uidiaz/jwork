299b56dc179e9959b515fc53f5ffbeddc487bab1
mailmanoriginal <mailmanoriginal@gmail.com>
2021-03-20 09:35:04 +0700

    Modul 2 Pagi

---
+++ DatabaseRecruiter.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class DatabaseRecruiter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatabaseRecruiter
+{
+    private String[] listRecuiter;
+
+    public DatabaseRecruiter()
+    {
+    }
+
+    public boolean addRecruiter(Recruiter recruiter)
+    {
+        return false;
+    }
+
+    public boolean removeRecruiter(Recruiter recruiter)
+    {
+        return false;
+    }
+
+    public Recruiter getRecruiter()
+    {
+        return null;
+    }
+
+    public String[] getListRecruiter()
+    {
+        return null;
+    }
+}
