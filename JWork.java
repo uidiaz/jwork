76b96f36a0a2104ffd69f65651870a50da80e13d
mailmanoriginal <mailmanoriginal@gmail.com>
2021-04-10 09:39:07 +0700

    PT Modul 5

--- JWork.java
+++ JWork.java
@@ -13,7 +13,7 @@ public class JWork
         Location location1 = new Location("DKI Jakarta", "Jakarta Pusat", "Apart");
         Recruiter recruiter1 = new Recruiter(1, "Diaz", "diaz.ilyasa@ui.ac.id", "08117776666", location1);
         Job job1 = new Job(2, "Senior Designer", recruiter1, 22000, JobCategory.WebDeveloper);
-        // Jobseeker jobseeker1 = new Jobseeker(3, "Adam", "Adam.Adam@ui.ac.id", "passworddddd", "18 Maret 2021");
+        Jobseeker jobseeker1 = new Jobseeker(3, "Adam", "Adam.Adam@ui.ac.id", "passworddddd", "18 Maret 2021");
         Bonus bonus1 = new Bonus(11, "BonusPay", 20000, 10000, true);
         Bonus bonus2 = new Bonus(12, "BonusPay", 20000, 30000, true);
 
@@ -26,12 +26,12 @@ public class JWork
         System.out.println(jobseeker3.toString());
         System.out.println(jobseeker4.toString());
 
-        EwalletPayment e1 = new EwalletPayment(1, job1, jobseeker1, InvoiceStatus.Finished);
-        EwalletPayment e2 = new EwalletPayment(2, job1, jobseeker1, bonus1, InvoiceStatus.Finished);
-        EwalletPayment e3 = new EwalletPayment(3, job1, jobseeker1, bonus2, InvoiceStatus.Finished);
+        EwalletPayment e1 = new EwalletPayment(1, job1, jobseeker2, InvoiceStatus.Finished);
+        EwalletPayment e2 = new EwalletPayment(2, job1, jobseeker3, bonus1, InvoiceStatus.Finished);
+        EwalletPayment e3 = new EwalletPayment(3, job1, jobseeker4, bonus2, InvoiceStatus.Finished);
 
-        BankPayment bp1 = new BankPayment(8, job1, jobseeker1, InvoiceStatus.Finished, 0);
-        BankPayment bp2 = new BankPayment(9, job1, jobseeker1, InvoiceStatus.Finished, 9000);
+        BankPayment bp1 = new BankPayment(8, job1, jobseeker2, InvoiceStatus.Finished, 0);
+        BankPayment bp2 = new BankPayment(9, job1, jobseeker3, InvoiceStatus.Finished, 9000);
 
         e1.setTotalFee();
         e2.setTotalFee();
